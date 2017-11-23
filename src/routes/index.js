bb6ba720e79181d9d4825aea1e8983f69a51d03d
amanvojha <amanvojha@icloud.com>
2017-11-23 02:30:45 -0800

    Developed Front End

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,26 @@ module.exports = function(app){
 		res.render('Home');
 	});
 	
+	app.get('/Upload', function(req,res){
+		
+		res.render('Upload');
+	});
+	
+	app.get('/Comparison', function(req,res){
+		
+		res.render('Comparison');
+	});
+	
+	app.get('/api/processJSON', function(req,res){
+		
+		//API will be called from the upload page.
+		//Logic for inserting JSON in MongoDB and Processing file for clustoring 
+		//will be written here.
+		//After processing app.render('/Comparison') will be called.
+		
+		
+		
+		
+	});
+	
 };
